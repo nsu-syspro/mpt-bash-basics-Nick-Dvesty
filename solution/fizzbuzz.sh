12b6182333c6c42e9e392827c2aea5a7d5e1b449
Nick-Dvesty <genmeln05@gmail.com>
2024-05-20 02:57:27 +0700

    fix

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <number>"
-    exit 1
-fi
-
 n=$1
-
 for ((i=1; i<=n; i++)); do
     if ((i % 3 == 0)) && ((i % 5 == 0)); then
         echo "Fizz Buzz"
